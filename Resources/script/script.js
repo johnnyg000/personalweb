85d17da6d4b960605e662f51b12bd9d3c9351038
John Gillespie <gillespie.john93@gmail.com>
2019-02-24 12:08:36 +0200

    added a timeline to work history

--- Resources/script/script.js
+++ Resources/script/script.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
         if ($(window).scrollTop() >= 150) {
             $('nav').addClass('sticky');
             $('nav').addClass('animated fadeInDown');
+            $('container-page-down').addClass('animated fadeOut');
         }
         else {
             $('nav').removeClass('sticky');
